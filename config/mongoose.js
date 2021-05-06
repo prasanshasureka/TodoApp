1755e5a4c98ec48eea24b392d4595e0ad5434f7f
prasanshasureka <sureka.prasansha@yahoo.com>
2021-05-07 01:05:46 +0530

    use mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo_list_db');
+// mongoose.connect('mongodb://localhost/todo_list_db');
+mongoAtlasUri = 'mongodb+srv://prasansha:prasansha@cluster0.xjr2n.mongodb.net/todo_list_db?retryWrites=true&w=majority';
+
+try {
+    // Connect to the MongoDB cluster
+     mongoose.connect(
+      mongoAtlasUri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected")
+    );
+
+  } catch (e) {
+    console.log("could not connect");
+  }
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error connecting to db'));
